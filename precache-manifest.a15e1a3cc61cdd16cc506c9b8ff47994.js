e50ffbaef0cb5f135df1a4cfb5a1577e55d417c0
Shikhar Sahdev <Shikhar@Tuushaars-MacBook-Pro-2.local>
2019-08-26 03:20:47 +0530

    Updates

---
+++ precache-manifest.a15e1a3cc61cdd16cc506c9b8ff47994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e96571af83f5f01b8d71070adafb7a8",
+    "url": "/sketchyrhymes/index.html"
+  },
+  {
+    "revision": "3795af19b816184f3810",
+    "url": "/sketchyrhymes/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "5dc1ec5120e509a9ec29",
+    "url": "/sketchyrhymes/static/css/main.73f5acd5.chunk.css"
+  },
+  {
+    "revision": "3795af19b816184f3810",
+    "url": "/sketchyrhymes/static/js/2.b80ceb43.chunk.js"
+  },
+  {
+    "revision": "5dc1ec5120e509a9ec29",
+    "url": "/sketchyrhymes/static/js/main.e30c9dd0.chunk.js"
+  },
+  {
+    "revision": "c089f5477689da798ce0",
+    "url": "/sketchyrhymes/static/js/runtime~main.71e00f9b.js"
+  },
+  {
+    "revision": "216e6951d06b8f3002dffb550e6d1859",
+    "url": "/sketchyrhymes/static/media/header_mob.216e6951.png"
+  }
+]);
